206ec6f1261fed27287fb4ce7ca4ede8c624e858
nurulsiddik <nurulsiddik@379e0824-942c-0410-917a-69eafb750743>
2007-03-27 20:20:16 +0000

    new vo

---
+++ trunk/LuvCricket/src/wow/play/cricket/vo/UserTeamDetail_1VO.java
@@ -0,0 +1,116 @@
+/*
+ * UserTeamDetail_1VO.java
+ *
+ * Created on March 27, 2007, 11:45 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package wow.play.cricket.vo;
+
+/**
+ *
+ * @author NURUL SIDDIK
+ */
+public class UserTeamDetail_1VO
+{
+    
+    /** Creates a new instance of UserTeamDetail_1VO */
+    public UserTeamDetail_1VO()
+    {
+    }
+
+    /**
+     * Holds value of property user_team_id.
+     */
+    private String user_team_id;
+
+    /**
+     * Getter for property user_team_id.
+     * @return Value of property user_team_id.
+     */
+    public String getUser_team_id()
+    {
+        return this.user_team_id;
+    }
+
+    /**
+     * Setter for property user_team_id.
+     * @param user_team_id New value of property user_team_id.
+     */
+    public void setUser_team_id(String user_team_id)
+    {
+        this.user_team_id = user_team_id;
+    }
+
+    /**
+     * Holds value of property country_id.
+     */
+    private String country_id;
+
+    /**
+     * Getter for property country_id.
+     * @return Value of property country_id.
+     */
+    public String getCountry_id()
+    {
+        return this.country_id;
+    }
+
+    /**
+     * Setter for property country_id.
+     * @param country_id New value of property country_id.
+     */
+    public void setCountry_id(String country_id)
+    {
+        this.country_id = country_id;
+    }
+
+    /**
+     * Holds value of property country_status.
+     */
+    private String country_status;
+
+    /**
+     * Getter for property country_status.
+     * @return Value of property country_status.
+     */
+    public String getCountry_status()
+    {
+        return this.country_status;
+    }
+
+    /**
+     * Setter for property country_status.
+     * @param country_status New value of property country_status.
+     */
+    public void setCountry_status(String country_status)
+    {
+        this.country_status = country_status;
+    }
+
+    /**
+     * Holds value of property team_points.
+     */
+    private String team_points;
+
+    /**
+     * Getter for property team_points.
+     * @return Value of property team_points.
+     */
+    public String getTeam_points()
+    {
+        return this.team_points;
+    }
+
+    /**
+     * Setter for property team_points.
+     * @param team_points New value of property team_points.
+     */
+    public void setTeam_points(String team_points)
+    {
+        this.team_points = team_points;
+    }
+    
+}
